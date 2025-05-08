a7023a5497ed95decc0b410255855c6d87000666
DhanaRoobanR <dhanarooban@softworthsolutions.in>
2025-05-08 07:23:49 +0530

    Setup Husky, Lint, and Prettier

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,6 @@ const Header: React.FC = () => {
     <AppBar position="static" className="bg-gray-800 text-white shadow-md fixed top-0 left-0 right-0 z-50">
       <Toolbar>
         <Typography variant="h6">My React App</Typography>
-        {/* Add navigation links here */}
       </Toolbar>
     </AppBar>
   );
